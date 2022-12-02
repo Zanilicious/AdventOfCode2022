1d184c266f23a11b4f949bc4e2842504e39c46c8
Zanilicious <joshua0906@gmail.com>
2022-12-02 11:18:13 +0100

    updated file structure

--- Day 1/day_1_solution.py
+++ Day 1/day_1_solution.py
@@ -1,11 +1,13 @@
-# First day of Advent Of Code 2022
-# Problem formulation: Inputstream of series of numbers, find the max sequence
-
+# Input stored in file
+# Open file
 f = open("input_day_1.txt", "r")
 
+# Set up parameters
 line = " "
 int_array = [0]
 i = 0
+# Read line by line, add inputs into array until empty space
+# New array entry starts following that
 while line != "":
     line = f.readline()
     if line == "":
@@ -16,12 +18,13 @@ while line != "":
     else:
         int_array[i] += int(line)
 
+# For problem two, simple solution of finding max and adding for top three max values
 a = max(int_array)
 del int_array[int_array.index(max(int_array))]
-
 b = max(int_array)
 del int_array[int_array.index(max(int_array))]
 c = max(int_array)
 del int_array[int_array.index(max(int_array))]
 
+# Present added max top three values
 print(a + b + c)
